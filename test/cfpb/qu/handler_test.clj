8e7a8b5c371a0f8a6560c9d2303fb781e8b3b411
Clinton N. Dreisbach <clinton.dreisbach@cfpb.gov>
2013-04-29 13:42:25 -0400

    Added tests for dataset handlers

--- test/cfpb/qu/handler_test.clj
+++ test/cfpb/qu/handler_test.clj
@@ -10,7 +10,7 @@
 
 (facts "about /data"
        (prerequisite (#'cfpb.qu.data/get-datasets) => [])
-       
+
        (fact "it returns successfully"
              (app (request :get "/data"))
              => (contains {:status 200
@@ -20,3 +20,20 @@
              => (contains {:status 200
                            :headers {"Content-Type" "application/xml;charset=UTF-8"}})))
 
+(facts "about /data/dataset"
+       (fact "it returns successfully when the dataset exists"
+             (prerequisite (#'cfpb.qu.data/get-metadata "good-dataset") => {})
+
+             (app (request :get "/data/good-dataset"))
+             => (contains {:status 200
+                           :headers {"Content-Type" "text/html;charset=UTF-8"}})
+
+             (app (request :get "/data/good-dataset.xml"))
+             => (contains {:status 200
+                           :headers {"Content-Type" "application/xml;charset=UTF-8"}}))
+
+       (fact "it returns a 404 when the dataset does not exist"
+             (prerequisite (#'cfpb.qu.data/get-metadata "bad-dataset") => nil)
+
+             (app (request :get "/data/bad-dataset"))
+             => (contains {:status 404})))
