f819b01d70e5e9bdc0f658518771cc99d9f967e9
Clinton N. Dreisbach <clinton.dreisbach@cfpb.gov>
2013-06-13 10:12:20 -0400

    Fix all tests

--- test/integration/handler_test.clj
+++ test/integration/handler_test.clj
@@ -17,30 +17,35 @@
        (fact "it returns successfully"
              (app (request :get "/data"))
              => (contains {:status 200
-                           :headers {"Content-Type" "text/html;charset=UTF-8"}})
+                           :headers {"Content-Type" "text/html;charset=UTF-8"
+                                     "Vary" "Accept"}})
 
              (app (request :get "/data.xml"))
              => (contains {:status 200
-                           :headers {"Content-Type" "application/xml;charset=UTF-8"}})))
+                           :headers {"Content-Type" "application/xml;charset=UTF-8"
+                                     "Vary" "Accept"}})))
 
   (facts "about /data/county_taxes"
          (fact "it returns successfully"
              (app (request :get "/data/integration_test"))
              => (contains {:status 200
-                           :headers {"Content-Type" "text/html;charset=UTF-8"}})
+                           :headers {"Content-Type" "text/html;charset=UTF-8"
+                                     "Vary" "Accept"}})
 
              (app (request :get "/data/integration_test.xml"))
              => (contains {:status 200
-                           :headers {"Content-Type" "application/xml;charset=UTF-8"}})))
+                           :headers {"Content-Type" "application/xml;charset=UTF-8"
+                                     "Vary" "Accept"}})))
 
   (facts "about /data/bad-dataset"
          (fact "it returns a 404"
                (app (request :get "/data/bad-dataset"))
                => (contains {:status 404
-                             :headers {"Content-Type" "text/html"
+                             :headers {"Content-Type" "text/html;charset=UTF-8"
                                        "Vary" "Accept"}})
 
                (app (request :get "/data/bad-dataset.xml"))
                => (contains {:status 404
-                             :headers {"Content-Type" "application/xml;charset=UTF-8"}}))))
+                             :headers {"Content-Type" "application/xml;charset=UTF-8"
+                                       "Vary" "Accept"}}))))
 
